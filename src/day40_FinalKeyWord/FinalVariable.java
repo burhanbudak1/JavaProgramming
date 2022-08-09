5745c62c2650dbe01548ec6a2129bec53ee17e3e
Burhan <bbudak035@gmail.com>
2022-08-10 01:28:00 +0300

    package day38_Inheritance.carTask;

--- src/day40_FinalKeyWord/FinalVariable.java
+++ src/day40_FinalKeyWord/FinalVariable.java
@@ -2,7 +2,30 @@ package day40_FinalKeyWord;
 
 public class FinalVariable {
 
+    final String birthDay;
+
+    public FinalVariable(String birthDay) {
+        this.birthDay = birthDay;
+    }
+
     public static void main(String[] args) {
+
+        final double pi = 3.14;
+
+
+        final String name;
+
+        name = "Java";
+
+        // name = "C";
+
+        System.out.println(name);
+
+        System.out.println("------------------------");
+
+        FinalVariable obj = new FinalVariable("May/01");
+
+        System.out.println(obj.birthDay);
 
 
 
