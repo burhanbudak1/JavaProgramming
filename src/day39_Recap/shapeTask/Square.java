b66c5450bc40d8d3fcfeefa9dc462edccc30bc3e
Burhan <bbudak035@gmail.com>
2022-08-09 03:53:31 +0300

    package day38_Inheritance.carTask;

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -16,11 +16,27 @@ public class Square extends Shape{
         this.side = side;
     }
 
-    public Square(String name, double side) {
+    public Square( double side) {
         super("Square");
         setSide(side);
     }
 
+    @Override
+    public double area() {
+        return side * side;
+    }
 
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
 
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
